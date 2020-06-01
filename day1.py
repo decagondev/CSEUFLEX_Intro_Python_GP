5abc1acce07adaf3cd0f43721f5892216f9c18ca
Tom Tarpey <tomtarpeydev@gmail.com>
2020-06-01 10:21:17 +0100

    print and starting lists

--- day1.py
+++ day1.py
@@ -1,14 +1,25 @@
 # This is a comment
 
 # lets print a string
-
+# print("Hello, CSEUFLEX!", "Some other text", "and theres more...")
 # variables
 # label = value
+# let const var (js)
+# int bool short (c)
+# first_name = "Tom"
+# # print("Hello CSEUFLEX and" , first_name)
+# num = 23.87
 
-# print("Hello CSEU5 and " + first_name)
+# # f strings
+# my_string = "    this is a string tom"
+# print(my_string)
 
+# print(my_string.strip())
+# print(len(my_string))
+# print(len(my_string.strip()))
 
-# f strings
+# print(f"Hello CSEUFLEX and                {first_name}.......")
+# print("something on a new line")
 
 
 
@@ -16,10 +27,15 @@
 # collections
 
 # create an empty list? Array
+lst1 = []
+lst_empty = list()
 
+print(my_list)
 
 # create a list with numbers 1, 2, 3, 4, 5
+lst2 = [1, 2, 3, 4, 5]
 
+print(lst2)
 
 # add an element 24 to lst1
 
