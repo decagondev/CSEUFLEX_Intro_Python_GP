d6963a0734aa9072db29dc74be6843678e20ee55
Tom Tarpey <tomtarpeydev@gmail.com>
2020-06-02 11:09:30 +0100

    guessing game first pass

--- guessing.py
+++ guessing.py
@@ -11,3 +11,20 @@
 
 # what is a REPL?
 # how can we make this game replayable? 
+
+
+#repl
+playing = True
+number = 45
+while playing: # LOOP
+    val = int(input(" Guess the number i am thinking of >>> ")) # READ
+
+    if val == number: # EVAL
+        print("You win") # PRINT
+        playing = False
+    else:
+        print("Not correct!!")
+
+# possible improvements
+# random number to start?
+# # maybe use ints and cast our guess
