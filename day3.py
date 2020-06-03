a50716d3d7e154c7d827aaca1632c9cb444b66a4
Tom Tarpey <tomtarpeydev@gmail.com>
2020-06-03 09:24:05 +0100

    oop class from js to python

--- day3.py
+++ day3.py
@@ -2,3 +2,26 @@
 
 # holds data
 # methods to act upon that data
+
+# class called Vec2
+# hold x and y as integers
+# constructor that can take in x and y
+
+# class Vec2 {
+#     constructor(x, y) { 
+#         this.x = x;
+#          this.y = y;
+#     } 
+# }
+
+# v = new Vec2(12, 23);
+
+# this keyword === self keyword
+
+class Vec2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    
+v1 = Vec2(12, 45)
