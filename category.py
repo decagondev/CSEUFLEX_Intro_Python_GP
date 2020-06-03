7101c4cd86bc3aa0021acc239a6b06fe34d864c2
Tom Tarpey <tomtarpeydev@gmail.com>
2020-06-03 10:25:51 +0100

    store categories

---
+++ category.py
@@ -0,0 +1,7 @@
+class Category:
+
+    def __init__(self, name): #, products):
+        self.name = name
+
+    def __str__(self):
+        return f"There are no products in {self.name}"
--- store.py
+++ store.py
@@ -1,3 +1,11 @@
+from category import Category
+
+# my categories
+fiction = Category("Fiction")
+non_fiction = Category("Non Fiction")
+golf_balls = Category("Golf Balls")
+other = Category("Some other Cat")
+
 # store class that has a name and a categories
 class Store:
     def __init__(self, name, categories):
@@ -7,12 +15,21 @@ class Store:
     def __str__(self):
         output = f"{self.name}\n"
         for i, c in enumerate(self.categories):
-            output += "   " + str(i + 1) + ": " + c + "\n"
+            output += "   " + str(i + 1) + ": " + c.name + "\n"
         
         output += "   " + str(i + 2) + ": Exit"
         return output
 
-s = Store("Books n thingz", ["Fiction", "Non Fiction", "Golf Balls", "Some other Cat"])
+s = Store("Books n thingz", [fiction, non_fiction, golf_balls, other])
 
-print(s) # self.__repr__() => self.__str__()
+# shop_open = True
+selection = 0
+while selection != len(s.categories) + 1:
+    print(s) # self.__repr__() => self.__str__()
+
+    selection = int(input("Select a category. "))
+    if selection == len(s.categories) + 1:
+        print("Thanks for shopping")
+    else:
+        print(f"{s.categories[selection - 1]}")
 
